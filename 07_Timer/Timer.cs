0653a7b913f00be6cbafec02e8047a1e14a65eaa
Nicolai <nico8829@edu.eal.dk>
2016-09-20 10:13:38 +0200

    done

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _07_Timer
+{
+    public class Timer
+    {
+        internal int Seconds;
+        public override string ToString()
+        {
+            TimeSpan ZeroSeconds = TimeSpan.FromSeconds(Seconds);
+            return string.Format("{0:D2}:{1:D2}:{2:D2}",ZeroSeconds.Hours, ZeroSeconds.Minutes, ZeroSeconds.Seconds);
+        }
+    }
+}
